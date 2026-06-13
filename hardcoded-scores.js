b5a54e7b4ce7bc119f735349abee487593036ddc
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-13 22:07:10 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -14,7 +14,7 @@ const HARDCODED_MATCH_SCORES = {
 
   // Group B
   'g-B-0-0': { homeScore: 1, awayScore: 1 }, // CAN vs BIH
-  'g-B-0-1': { homeScore: , awayScore:  }, // QAT vs SUI
+  'g-B-0-1': { homeScore: 1, awayScore: 1 }, // QAT vs SUI
   'g-B-1-0': { homeScore: , awayScore:  }, // SUI vs BIH
   'g-B-1-1': { homeScore: , awayScore:  }, // CAN vs QAT
   'g-B-2-0': { homeScore: , awayScore:  }, // SUI vs CAN
