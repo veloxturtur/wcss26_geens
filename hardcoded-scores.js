acc83b27194cffa8881a157e4b6a33533e537a34
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-25 07:28:23 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -9,7 +9,7 @@ const HARDCODED_MATCH_SCORES = {
   'g-A-0-1': { homeScore: 2, awayScore: 1 }, // KOR vs CZE
   'g-A-1-0': { homeScore: 1, awayScore: 0 }, // MEX vs KOR
   'g-A-1-1': { homeScore: 1, awayScore: 1 }, // RSA vs CZE
-  'g-A-2-0': { homeScore: 0, awayScore: 3 }, // CZE vs MEX
+  'g-A-2-0': { homeScore: 3, awayScore: 0 }, // MEX vs CZE
   'g-A-2-1': { homeScore: 1, awayScore: 0 }, // RSA vs KOR
 
   // Group B (CAN, BIH, QAT, SUI)
@@ -23,7 +23,7 @@ const HARDCODED_MATCH_SCORES = {
   // Group C (BRA, MAR, HTI, SCO)
   'g-C-0-0': { homeScore: 1, awayScore: 1 }, // BRA vs MAR
   'g-C-0-1': { homeScore: 1, awayScore: 0 }, // HTI vs SCO
-  'g-C-1-0': { homeScore: 3, awayScore: 0 }, // BRA vs HTI
+  'g-C-1-0': { homeScore: 0, awayScore: 3 }, // HTI vs BRA
   'g-C-1-1': { homeScore: 1, awayScore: 0 }, // MAR vs SCO
   'g-C-2-0': { homeScore: 0, awayScore: 3 }, // SCO vs BRA
   'g-C-2-1': { homeScore: 4, awayScore: 2 }, // MAR vs HTI
