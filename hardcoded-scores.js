2c30ec30baa7354906e31c21a9b65194fd5d5b3d
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-29 17:41:27 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -124,7 +124,7 @@ const HARDCODED_MATCH_SCORES = {
   'ko-r32-16': { stage: 'r32', date: '2026-07-03T19:00', home: 'ARG', away: 'CPV', homeScore: null, awayScore: null },
 
   // Round of 16 (July 4 – July 7, 2026)
-  'ko-r16-1': { stage: 'r16', date: '2026-07-04T19:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
+  'ko-r16-1': { stage: 'r16', date: '2026-07-04T19:00', home: 'CAN', away: 'TBD', homeScore: null, awayScore: null },
   'ko-r16-2': { stage: 'r16', date: '2026-07-04T22:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
   'ko-r16-3': { stage: 'r16', date: '2026-07-05T19:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
   'ko-r16-4': { stage: 'r16', date: '2026-07-05T22:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
