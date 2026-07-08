ddd8b05f6ea1ea7c57079627e37c62e6d8588466
fybsie <lien.geens@gmail.com>
2026-07-08 20:54:19 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -134,10 +134,10 @@ const HARDCODED_MATCH_SCORES = {
   'ko-r16-8': { stage: 'r16', date: '2026-07-07T22:00', home: 'ARG', away: 'EGY', homeScore: 3, awayScore: 2 },
 
   // Quarterfinals (July 9 – July 10, 2026)
-  'ko-qf-1': { stage: 'qf', date: '2026-07-09T19:00', home: 'MAR', away: 'TBD', homeScore: null, awayScore: null },
-  'ko-qf-2': { stage: 'qf', date: '2026-07-09T22:00', home: 'FRA', away: 'TBD', homeScore: null, awayScore: null },
-  'ko-qf-3': { stage: 'qf', date: '2026-07-10T19:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
-  'ko-qf-4': { stage: 'qf', date: '2026-07-10T22:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
+  'ko-qf-1': { stage: 'qf', date: '2026-07-09T19:00', home: 'MAR', away: 'FRA', homeScore: null, awayScore: null },
+  'ko-qf-2': { stage: 'qf', date: '2026-07-09T22:00', home: 'ESP', away: 'BEL', homeScore: null, awayScore: null },
+  'ko-qf-3': { stage: 'qf', date: '2026-07-11T22:00', home: 'NOR', away: 'ENG', homeScore: null, awayScore: null },
+  'ko-qf-4': { stage: 'qf', date: '2026-07-12T02:00', home: 'ARG', away: 'SUI', homeScore: null, awayScore: null },
 
   // Semifinals (July 14 – July 15, 2026)
   'ko-sf-1': { stage: 'sf', date: '2026-07-14T20:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
